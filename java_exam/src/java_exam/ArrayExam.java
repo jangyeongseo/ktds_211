b8abd80699b0daf2d491b1ba1b943d403c495f53
jangyeongseo <jyeongseo82@gmail.com>
2026-01-19 17:03:58 +0900

    26.01.19

---
+++ java_exam/src/java_exam/ArrayExam.java
@@ -0,0 +1,19 @@
+package java_exam;
+
+public class ArrayExam {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] scoreArray = new int[7]; 
+		// new : 참조형 변수(Reference type)를 만들어라
+		// String 도 참조형 변수
+		// scoreArray.length -> . : 연산자 / 객체의 속성/메서드 접근 연산자
+		// 0|0|0|0|0|0|0
+		System.out.println(scoreArray);
+
+		int score = 60;
+		System.out.println(score);
+
+	}
+
+}
